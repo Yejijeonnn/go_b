d5855a532aebcc5dacc9b9a450c62dc5bb1723d7
YejiJeonnn <202344046@itc.ac.kr>
2023-11-06 17:37:53 +0900

    week11 v2.5

--- week11/main.go
+++ week11/main.go
@@ -10,15 +10,30 @@ func main() {
 	test := [5]bool{true, true, true} // 초기화하지 않은 배열 원소의 제로 값은 해당 원소의 타입의
 	fmt.Println(test[3])
 
+	// for prime := range primes {	// 소수 값을 배열에서 꺼내려고 하지만 인덱스가 출력
+	for _, prime := range primes {
+		fmt.Println(prime)
+	}
+
+	// for idx, prime := range primes {
+	// 	fmt.Println(idx, prime)
+	// }
+
 	// for i := 0; i < 3; i++ {
 	// 	fmt.Println(primes[i])
 	// }
 
-	i := 0
-	for i < 3 {
-		fmt.Println(primes[i])
-		i++
-	}
+	// i := 0
+	// for i < 3 {
+	// 	fmt.Println(primes[i])
+	// 	i++
+	// }
+
+	// i := 0
+	// for i < len(primes) {
+	// 	fmt.Println(primes[i])
+	// 	i++
+	// }
 
 	// var primes [3]int = [3]int{2, 3, 5}		// 배열 리터럴로 primes 배열 초기화
 	// fmt.Println(primes, primes[1])
