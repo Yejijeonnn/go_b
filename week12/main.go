e47e3854da76889dd99e0d37eac9ae7a00baa212
YejiJeonnn <202344046@itc.ac.kr>
2023-11-13 17:52:16 +0900

    week12 v3.2

--- week12/main.go
+++ week12/main.go
@@ -10,7 +10,12 @@ func main() {
 	as := a[0:2]
 	as[1] = "Z"
 	c := append(a, "y")
+	// c := append(a, "y", "x")
+
+	c[0] = "q"
 
 	fmt.Println(a, len(a), cap(a))
 	fmt.Println(c, len(c), cap(c))
+	fmt.Printf("%x %x %x\n", &a[0], &as[0], &c[0]) // c만 주소가 다름 왜? -> 용량이 더 필요하니까 -> 그렇다면 c의 주소는 a, as와 같을 수 없음
+
 }
