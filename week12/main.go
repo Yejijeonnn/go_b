c171d2f84f34d7b40f0c1819a607a40d54790c36
YejiJeonnn <202344046@itc.ac.kr>
2023-11-13 17:32:45 +0900

    week12 v3.1

--- week12/main.go
+++ week12/main.go
@@ -3,26 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	a := []string{"a", "b", "c", "d"}
+	as := a[:2]
+	as[1] = "Z"
+	fmt.Println(a, as)
 
-	// s := make([]int, 5)  // 단축변수 사용, 변수 선언과 동시에 메모리 할당, 원소들은 제로 값으로 초기화
-
-	s := []int{0, 0, 0, 0, 0} // 단축연산자 사용 및 변수 선언과 동시에 메모리 할당 그리고 원소 초기화
-
-	s[4] = 100
-	s[0] = 7
-	s[1] = 91
-
-	for _, value := range s {
-		fmt.Println(value)
-	}
-
-	copyS := s[1:4]
-	for _, value := range copyS {
-		fmt.Println(value)
-	}
-
-	test := [3]string{"inha", "go", "student"}
-	testS := test[:2] // testS := test[0:2]
-	fmt.Println(test, len(test))
-	fmt.Println(testS, len(testS))
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(b, bs)
 }
