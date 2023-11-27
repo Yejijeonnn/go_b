82ac4d99dedb2bafc7e0fa3862f0a3e55a6f299c
YejiJeonnn <202344046@itc.ac.kr>
2023-11-27 17:41:36 +0900

    week14 v4.1

--- week14/main.go
+++ week14/main.go
@@ -1,57 +1,24 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
-// GetStrings reads a string from each line of a file.
-func GetStrings(fileName string) ([]string, error) {
-	var lines []string
-	file, err := os.Open(fileName)
-	if err != nil {
-		return nil, err
+func status(name string) {
+	balls := map[string]int{"성기훈": 20, "오일남": 0}
+	// ball := balls[name]
+	ball, exists := balls[name]
+	// fmt.Println(ball, exists)
+
+	if !exists {
+		fmt.Println(name, "님은 게임에 참여하실 수 없습니다.")
+	} else if ball < 1 {
+		fmt.Println(name, "님은 ", balls[name], "개로 탈락..")
+	} else {
+		fmt.Println(name, "님은 게임에서 승리하셨습니다.")
 	}
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() { // 입력하는만큼 반복
-		line := scanner.Text()
-		lines = append(lines, line)
-	}
-	err = file.Close()
-	if err != nil {
-		return nil, err
-	}
-	if scanner.Err() != nil {
-		return nil, scanner.Err()
-	}
-	return lines, nil
 }
 
 func main() {
-	// var games map[int]string
-	// games = make(map[int]string)
-	games := map[int]string{
-		456: "성기훈",
-		218: "박해수",
-		067: "강새벽",
-		001: "오일남",
-		199: "알리",
-		101: "아이오아이",
-	}
-
-	// fmt.Println(games[067])
-	for _, v := range games {
-		fmt.Println(v)
-	}
-
-	// update
-	games[101] = "장덕수"
-
-	// delete
-	delete(games, 199)
-
-	for k, v := range games {
-		fmt.Println(k, v)
-	}
+	status("오일남")
+	status("강철")
+	status("성기훈")
 }
